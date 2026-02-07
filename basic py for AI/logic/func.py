ae3b0bc3081cf0841447b9766bcb5a7a9b65ad77
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2026-02-07 23:34:07 +0700

    Learn Lambda Function but haven't questions

--- basic py for AI/logic/func.py
+++ basic py for AI/logic/func.py
@@ -1,30 +1,37 @@
 # LEARN ALL ABOUT FUNCTION 
 
-# 1. LAMBDA FUNCTIONS (Syntax = lambda arguments : expression)
+# --- 1. LAMBDA FUNCTIONS (Syntax = lambda arguments : expression) ---
 
 # Example from lambda function
 x = lambda y : y + 10       # Lambda with single parameter
 z = lambda x, y : x * y     # Lambda with multi parameter
 
 
-# The power of lambda is better shown when you use them as an anonymous function inside another function.
-def myFunc(x) : 
-    return lambda y : y * x
+                            
+def myFunc(x) :              # The power of lambda is better shown when you -
+    return lambda y : y * x  # use them as an anonymous function inside another function.
 
 nextFunc = myFunc(5)
 print(nextFunc(10))
 
-# 2. LAMBDA FUNCTION WITH BUILT-IN FUNCTIONS
+# --- 2. LAMBDA FUNCTION WITH BUILT-IN FUNCTIONS ---
 
 # Lambda functions are commonly used with built-in functions like map(), filter(), and sorted().
 
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]                 # Make a List For Example
+some = ["Apple", "Banana", "Mango", "Strawberry"]    # Make a List in String
+
 # 1. Using Lambda with map()
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-doubled = list(map(lambda x : x * 2, number))  # This function will doubled a number on the list.
+doubled = list(map(lambda x : x * 2, number))        # This function Will doubled a number on the list.
 print(doubled)    
 
-# 2. 
+# 2. Using Lambda with filter()
+ganjil = list(filter(lambda y : y % 2 != 0, number)) # This function Will filter which number can divided by 2
+print(ganjil)
 
+# 3. Using Lambda with sorted()
+words = sorted(some, key = lambda x : len(x))        # This Function Will Sorted a List by String
+print(words)
 
 
 
