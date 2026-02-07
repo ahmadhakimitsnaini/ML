d8c43e21db2fb80fd9d26ff74c7a820d66c4b900
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2026-02-07 19:40:03 +0700

    Logic Exercise Soal 2: Klasifikasi Data Sensor

--- basic py for AI/dictionary/logic/logic.py
+++ basic py for AI/dictionary/logic/logic.py
@@ -1,19 +1,16 @@
 # EXERCISE PYTHON LOGIC
 
+# Soal 1: Logika Akses (Authentication)
 """
-    Soal 1: Logika Akses (Authentication)
-    Buatlah fungsi cek_akses(username, role) yang menerima dua parameter string.
-
-    Jika role adalah "admin", cetak "Akses penuh diberikan".
-
-    Jika role adalah "user" dan username tidak kosong, cetak "Akses terbatas diberikan".
-
-    Selain itu, cetak "Akses ditolak".
-    (Latihan ini relevan untuk rencana Anda membangun sistem login atau dashboard e-commerce)
+    1. Buatlah fungsi cek_akses(username, role) yang menerima dua parameter string.
+    2. Jika role adalah "admin", cetak "Akses penuh diberikan".
+    3. Jika role adalah "user" dan username tidak kosong, cetak "Akses terbatas diberikan".
+    4. Selain itu, cetak "Akses ditolak".
+       (Latihan ini relevan untuk rencana Anda membangun sistem login atau dashboard e-commerce)
 """
 
-username = str(input("Masukkan Username       : "))
-role =     str(input ("Masukkan Role Pengguna  : "))
+# username = str(input("Masukkan Username       : "))
+# role =     str(input ("Masukkan Role Pengguna  : "))
 def auth(username, role) : 
     if role == "Admin" or role == "admin" :
         print("Akses penuh diberikan")
@@ -22,4 +19,26 @@ def auth(username, role) :
     else :
         print("Akses Di Tolak")
 
-auth(username, role)
+# auth(username, role)
+
+
+# Soal 2: Klasifikasi Data Sensor
+"""
+    Dalam proyek IT, seringkali kita menerima data sensor. Buatlah logika untuk mengklasifikasikan suhu:
+
+    1. Suhu < 0     : "Membeku"
+    2. Suhu 0 - 30  : "Normal"
+    3. Suhu > 30    : "Panas"
+    Tambahkan pengecekan: Jika suhu di atas 100, cetak "Peringatan: Sensor Rusak!".
+"""
+inputSuhu = int(input ("Masukkan suhu : "))
+if inputSuhu < 0 :
+    print("Membeku")
+elif inputSuhu <= 30 :
+    print("Normal")
+elif inputSuhu >= 30 and inputSuhu <= 100 :
+    print("Panas")
+elif inputSuhu > 100:
+    print("Peringatan : Sensor Rusak!")
+else : 
+    print("Maaf suhu tidak terdeteksi")
