a351fdc158cf09ab48ee8c1e4ad057a110ccd288
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2026-02-08 23:14:17 +0700

    Lambda Quiz Bagian 2: Studi Kasus (Logika AI)

--- basic py for AI/logic/quizFunc.py
+++ basic py for AI/logic/quizFunc.py
@@ -18,3 +18,14 @@ suhu_ruangan = [25, 32, 18, 40, 22, 35]
 filterSuhu = list(filter(lambda y : y > 30, suhu_ruangan))  # Using filter for searching number greather than 30
 print(filterSuhu)
 
+# Bagian 2: Studi Kasus (Logika AI)
+"""
+    Sorting Data Kompleks: Diberikan list berisi koordinat titik dalam bentuk tuple :
+    titik = [(1, 2), (4, 1), (2, 10), (10, 5)]. 
+    Gunakan fungsi sort() atau sorted() bersama dengan lambda sebagai kunci (key) 
+    untuk mengurutkan list tersebut berdasarkan elemen kedua (nilai y) dari masing-masing tuple.
+"""
+titik = [(1, 2), (4, 1), (2, 10), (10, 5)]
+sorting = sorted(titik, key = lambda x : x[1])
+print(sorting)
+
