05be221d0af07ce59edcf7b5e6dcbe04d89a6851
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2026-02-24 22:02:47 +0700

    Refactor : TUGAS 1: Buat fungsi untuk menghitung jarak antara 2 titik

--- main/solve.py
+++ main/solve.py
@@ -18,3 +18,17 @@ data_historis = [
 
 # 2. DATA BARU (Data sensor yang baru masuk dan belum tahu statusnya)
 sensor_baru = [35.0, 32.0]
+
+# =====================================================================
+# TUGAS 1: Buat fungsi untuk menghitung jarak antara 2 titik
+# Rumus Euclidean Distance: Akar dari ((suhu1 - suhu2)^2 + (kel1 - kel2)^2)
+# =====================================================================
+def hitung_jarak(titik1, titik2):
+    suhu1, kel1 = titik1
+    suhu2, kel2 = titik2
+    
+    # TODO 1: Tulis rumus matematika di bawah ini untuk mencari jarak!
+    # Gunakan math.sqrt() untuk akar kuadrat, dan ** 2 untuk pangkat dua.
+    jarak = 0.0 # <--- UBAH BARIS INI
+    
+    return jarak
